1254e46c029a6f27c066a543349f052133f6276f
anderson03AyA <andersonsolarte03@gmail.com>
2023-03-09 14:38:35 -0500

    icon-close

--- js/menu_nav.js
+++ js/menu_nav.js
@@ -1,25 +1,24 @@
 (() => {
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
-  
-  
+
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("header-mobile");
     //oculta etiquetas sobrantes a del nav
-    
+
     document.querySelector(".logo").style.display = "none";
     document.querySelector(".nav-menu.ul").style.display = "flex";
     document.querySelector(".contacts-menu").style.display = "flex";
     //--------------
     const scrollLockMethod = !isMenuOpen
-    ? "disableBodyScroll"
-    : "enableBodyScroll";
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
   };
-  
+
   openMenuBtn.addEventListener("click", toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
@@ -31,4 +30,11 @@
   });
 })();
 
+//para cambiar el icono menu
 
+function toggleMenuIcon() {
+  var icons = document.querySelectorAll(".icon, .icon-x");
+  icons.forEach(function (icon) {
+    icon.classList.toggle("active");
+  });
+}
